derzahla/zvio-middleware

If alert has simply moved during failover, do not send it

src/middlewared/middlewared/plugins/alert.py
@@ -206,6 +206,13 @@ class AlertService(Service):
         for policy_name, policy in self.policies.items():
             gone_alerts, new_alerts = policy.receive_alerts(now, self.alerts)
 
+            for gone_alert in list(gone_alerts):
+                for new_alert in new_alerts:
+                    if gone_alert.source == new_alert.source and gone_alert.key == new_alert.key:
+                        gone_alerts.remove(gone_alert)
+                        new_alerts.remove(new_alert)
+                        break
+
             for alert_service_desc in await self.middleware.call("datastore.query", "system.alertservice"):
                 service_settings = dict(default_settings, **alert_service_desc["settings"])
 

bsdnas/middleware

(cherry picked from commit bb953f6fa666f348088b15fd390caec09db65ce0)

src/middlewared/middlewared/plugins/alert.py
@@ -321,6 +321,12 @@ class AlertService(Service):
                         classes.get(alert.klass.name, {}).get("policy", DEFAULT_POLICY) == policy_name
                     )
                 ]
+                for gone_alert in list(service_gone_alerts):
+                    for new_alert in service_new_alerts:
+                        if gone_alert.klass == new_alert.klass and gone_alert.key == new_alert.key:
+                            service_gone_alerts.remove(gone_alert)
+                            service_new_alerts.remove(new_alert)
+                            break
 
                 if not service_gone_alerts and not service_new_alerts:
                     continue